007f6e8f460eb7c90df26831fe093749a1ab9c39
Денис Пономарев <deskin196@gmail.com>
2023-06-15 11:13:48 +0300

    Добавил функцию редактирования записи

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def show_menu():
     print('\t 5. Добавить новую запись')
     print('\t 6. Удалить запись')
     print('\t 7. Изменить запись')
-    return int(input('Введите пункт меню: '))
+    return int(input('Введите пункт меню, для выхода из программы введите 8: '))
 
 def get_new_user():
     user_data = []
@@ -29,8 +29,42 @@ def get_new_user():
     user_data.append(get_string_from_console('Введите описание: '))
     return user_data
 
+#Функция редактирования записи, принимает массив с элементами записи (id, Фамилия, имя ...)
+def get_edit_user(user_data):
+
+    surname = get_string_from_console(f'Введите фамилию или оставьте пустым для сохранения старого значения ({user_data[1]}): ')
+    if(surname != ''):  user_data[1] = surname
+    
+    name = get_string_from_console(f'Введите имя или оставьте пустым для сохранения старого значения ({user_data[2]}): ')
+    if(name != ''):  user_data[2] = name
+
+    telephone = get_string_from_console(f'Введите номер телефона или оставьте пустым для сохранения старого значения ({user_data[3]}): ')
+    if(telephone != ''):  user_data[3] = telephone
+
+    description = get_string_from_console(f'Введите описание или оставьте пустым для сохранения старого значения ({user_data[4]}): ')
+    if(description != ''):  user_data[4] = description
+
+    return user_data
+
 def get_string_from_console(text):
     return input(text).strip()
+
+def read_int(text, max, min):
+    while(True):
+        try:
+            #Ввод пытаемся преобразовать в число с плавающей точкой, на случай если ввели не целое число
+            readInt = float(input(text))
+        #Если при преобразовании в float возникло исключение, выводим ошибку и завершаем иттерацию цикла while
+        except:
+            print('Введено не число!')
+            continue
+        #Если попали сюда, значит всё нормально, проверяем равны ли числа типа float и преобразованное в int(откинули дробную часть если она была)
+        if  int(readInt) == readInt:
+            if readInt > max: print(f'Число должно быть не больше {max}')
+            elif readInt < min: print(f'Число должно быть не меньше {min}')
+            else: return int(readInt)
+        else:
+            print('Число должно быть, целым!')
 
 def find_string_in_column(phone_book, find_string, column):
     return [x for x in phone_book if str.upper(x[column]) == str.upper(find_string)]
@@ -98,9 +132,16 @@ def work_with_phonebook():
             phone_book = read_csv('phonebook.csv')
 
         elif choice == select_menu.DELETE_USER:
-            index = get_string_from_console('Введите номер записи для удаления: ')
+            index = read_int('Введите номер записи для удаления: ', len(phone_book), 1)
             write_csv('phonebook.csv', [i for i in phone_book if i[0] != index])
             phone_book = read_csv('phonebook.csv')
+
+        elif choice == select_menu.EDIT_USER:
+            index = read_int('Введите номер записи для редактирования: ', len(phone_book), 1)
+            phone_book[int(index) - 1] = get_edit_user(phone_book[int(index) - 1])
+            write_csv('phonebook.csv', phone_book)
+            phone_book = read_csv('phonebook.csv')
+
         choice = show_menu()
 
 work_with_phonebook()
